9b2a42bc3e3ea6dca58140e3fb265a9ceed489fa
DongOk <ehd85623@gmail.com>
2022-05-28 16:05:34 +0900

    changed buttons for stock list to use TMP

--- Assets/Scenes/Scene2/SecuritiesStockListAll.cs
+++ Assets/Scenes/Scene2/SecuritiesStockListAll.cs
@@ -2,18 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System.Globalization;
 
 using StockDOGE;
 
 public class SecuritiesStockListAll : MonoBehaviour
 {
+    NumberFormatInfo numberFormat;
 
     // Start is called before the first frame update
     void Start()
     {
+        numberFormat = new CultureInfo("ko-KR", false).NumberFormat;
+
         //GameObject btntemp = Resources.Load<GameObject>("Prefabs/ButtonContentForList");
         //GameObject Instancetemp = (GameObject)Instantiate(btntemp, GameObject.Find("Viewport").transform.Find("Content"));
         //Instancetemp.GetComponentInChildren<Text>().text = "NO ROOP BTN";
+
 
         for (int i = 0; i < GameObject.Find("Stocks").GetComponent<Stocks>().getStocksCount(); i++)
         {
@@ -23,12 +29,24 @@ public class SecuritiesStockListAll : MonoBehaviour
 
             //print(GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getName());
 
-            GameObject btn = Resources.Load<GameObject>("Prefabs/ButtonContentForList");
+            //GameObject btn = Resources.Load<GameObject>("Prefabs/ButtonContentForList");
+            //GameObject Instance = (GameObject)Instantiate(btn, GameObject.Find("Viewport").transform.Find("Content"));
+            //Instance.GetComponentInChildren<Text>().text = GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getName()
+            //    + "    "
+            //    + (int)(GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getPrice())
+            //    + "¿ø ";
+
+
+            GameObject btn = Resources.Load<GameObject>("Prefabs/StockButtonContentListAll");
             GameObject Instance = (GameObject)Instantiate(btn, GameObject.Find("Viewport").transform.Find("Content"));
-            Instance.GetComponentInChildren<Text>().text = GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getName()
-                + "    "
-                + (int)(GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getPrice())
-                + "¿ø ";
+            Instance.transform.Find("TextName").GetComponentInChildren<TextMeshProUGUI>().text = GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getName();
+            Instance.transform.Find("TextPrice").GetComponentInChildren<TextMeshProUGUI>().text = ((int)GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getPrice()).ToString("c", numberFormat);
+
+            //Instance.transform.Find("TextName").GetComponentInChildren<Text>().text = GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getName();
+            //Instance.transform.Find("TextPrice").GetComponentInChildren<Text>().text = (int)GameObject.Find("Stocks").GetComponent<Stocks>().getStockByIndex(i).getPrice()
+            //    + "\\";
+
+            //Instance.GetComponent<SpriteRenderer>().sortingLayerID = SortingLayer.NameToID("inAppContent");
 
 
         }
